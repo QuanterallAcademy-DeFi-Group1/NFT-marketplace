27ada510d5e85a39fff267123054cb1563636841
Nikolay Nikolaev <n.nikolaevbg97@gmail.com>
2022-06-25 14:08:16 +0300

    Changed jimp function

--- server/index.js
+++ server/index.js
@@ -30,23 +30,29 @@ const storage = multer.diskStorage({
 
 const imageUpload = multer({ storage: storage })
 
+app.get('/image-view/:name', (req, res) => {
+    res.sendFile(__dirname + `/public/watermarked-${req.query.name}`)
+})
+
 app.post('/image-upload', imageUpload.array("my-image-file"), (req, res) => {
     console.log('POST request received to /image-upload.');
     console.log('Axios POST body: ', req.body);
-    res.send('POST request recieved on server to /image-upload.');
 
-    let imgActive = `./public/nfts/${imagefilename}`;
-
-    Jimp.read(`./public/nfts/${imagefilename}`)
-        .then((tpl) => tpl.clone().write(imgActive))
-        .then(() => Jimp.read(imgActive))
-        .then((tpl) =>
-            Jimp.read(`./public/logo/logo.jpg`).then((logoTpl) => {
-                logoTpl.opacity(0.2)
-                return tpl.composite(logoTpl, 128, 128, [Jimp.BLEND_DESTINATION_OVER])
-            }),
-        )
-        .then((tpl) => tpl.write(`./public/watermarked-${imagefilename}`))
+    Jimp.read(`./public/nfts/${imagefilename}`, (err, fir_img) => {
+        if (err) {
+            console.log(err);
+        } else {
+            Jimp.read(`./public/logo/logo.jpg`, (err, sec_img) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    sec_img.opacity(0.2)
+                    fir_img.composite(sec_img, 12, 12, [Jimp.BLEND_DESTINATION_OVER])
+                    fir_img.write(`./public/watermarked-${imagefilename}`)
+                }
+            })
+        }
+    });
 })
 
 const port = 1337;
