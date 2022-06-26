84b30e28b40acb188c465b75153a95ba40de8555
thecarguy97 <filip.boshkov@gmail.com>
2022-06-26 15:46:07 +0200

    frontend header and connect wallet

---
+++ src/components/NFT/NFT.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NFT.scss';
+import {Box, Typography} from '@mui/material';
+
+const NFT = () => {
+    return (
+        <Box className="body">
+            <Box className='banner'>
+                <Typography className="text" variant='h2'>
+                    NFT
+                </Typography>
+            </Box>
+        </Box>
+    );
+}
+
+export default NFT
---
+++ src/components/Team/Team.js
@@ -0,0 +1,17 @@
+import { Typography, Box } from "@mui/material";
+import React from "react";
+import './Team.scss';
+
+const Team = () => {
+    return (
+        <Box className="body">
+            <Box className="banner">
+                <Typography className="text" variant="h2">
+                    Team
+                </Typography>
+            </Box>
+        </Box>
+    );
+}
+
+export default Team
